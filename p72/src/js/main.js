bec43c5af9b9b9e9a931dead898e652b62b3d080
Tom Vanhoutte <syntraprogrammeurs@gmail.com>
2025-11-10 11:07:05 +0100

    p72

--- p72/src/js/main.js
+++ p72/src/js/main.js
@@ -5,3 +5,37 @@ import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
 
 //eigen js
+document.addEventListener("DOMContentLoaded", () => {
+    const box = document.getElementById("rb_box");
+    const msg = document.getElementById("rb_msg");
+
+    // Hover
+    box.addEventListener("mouseover", () => {
+        box.classList.remove("bg-light");
+        box.style.setProperty("background-color", "lightgreen", "important");
+        msg.textContent = "👋 Hallo!";
+    });
+
+    // Verlaten
+    box.addEventListener("mouseleave", () => {
+        box.classList.add("bg-light");
+        box.style.removeProperty("background-color");
+        msg.textContent = "😭 Kom terug!";
+    });
+
+    // Klik
+    box.addEventListener("click", () => {
+        box.style.setProperty("border", "3px solid black", "important");
+        msg.textContent = "🖱️ Je klikt!";
+    });
+
+    // Dubbelklik
+    box.addEventListener("dblclick", () => {
+        box.style.setProperty("border", "3px solid red", "important");
+        msg.textContent = "⚡ Dubbelklik wow!";
+    });
+});
+
+
+
+
